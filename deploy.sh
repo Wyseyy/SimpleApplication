11b4ca07da93eb43f7dc62b2af42c16154e36d72
Wyseyy <eoinwysey@gmail.com>
2025-03-11 18:11:39 +0000

    updated a lot

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt install nodejs npm
+sudo npm install -g pm2
+pm2 stop SimpleApplication
+cd SimpleApplication/
+npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+pm2 start ./bin/www --name SimpleApplication
