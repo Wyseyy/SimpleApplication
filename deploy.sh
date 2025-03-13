d7b83f1b2b0682fff547ade074f689a3ca95d43a
Wyseyy <eoinwysey@gmail.com>
2025-03-13 14:02:08 +0000

    submission fix

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd SimpleApplication/
 npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
-pm2 start ./bin/www --name SimoleApplication
+pm2 start ./bin/www --name SimpleApplication
